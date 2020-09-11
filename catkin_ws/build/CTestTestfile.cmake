e90724b2ead06aad31334e67dfb3ed963c88674d
Jorge <jorgego1940@hotmail.com>
2020-09-11 11:26:47 +0100

    Primeira Atualização da Simulação do Gazebo

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/jorge/catkin_ws/src
+# Build directory: /home/jorge/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("meu_robo")
+subdirs("turtlebot_teleop")
---
+++ catkin_ws/build/meu_mundo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jorge/catkin_ws/src/meu_mundo
+# Build directory: /home/jorge/catkin_ws/build/meu_mundo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
